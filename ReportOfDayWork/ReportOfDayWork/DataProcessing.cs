90a09f48017bea31ee16d3dbb34c9da9a015ab7e
inkjett <mail22sms@gmail.com>
2020-05-11 21:42:03 +0500

    добавил вывод данных для всех офисов

--- ReportOfDayWork/ReportOfDayWork/DataProcessing.cs
+++ ReportOfDayWork/ReportOfDayWork/DataProcessing.cs
@@ -18,17 +18,17 @@ namespace ReportOfDayWork
                 arrayOfPeopleWorkTime.Add(new PeopleWorkTimeDay(arrayOfuser[u].FullName, null, null, null, null));// записываем ФИО пользователя
                 for (var w = 0; w < arrayOfWorkTime.Count; w++) // идем по массиву с данными об отработаном времени 
                 {
-                    if ((arrayOfWorkTime[w].ReaderId == 3) && (arrayOfuser[u].Сardnum == arrayOfWorkTime[w].Cardnum))// проверка прихода на работу 3-вход 13-выход
-                    {
+                    if ((Variables.inOffice.ToList().IndexOf(arrayOfWorkTime[w].ReaderId) != -1) && (arrayOfuser[u].Сardnum == arrayOfWorkTime[w].Cardnum))// проверка прихода на работу денные о считывателях берем из массива inOffice
+                    {                        
                         arrayOfPeopleWorkTime[u].ComingToWork = arrayOfWorkTime[w].EventsDate;// добавляем время прихода на работу     
                         for (var j = 0; j < arrayOfWorkTime.Count; j++)// ищем выход с работы
                         {
-                            if ((arrayOfWorkTime[w].PeopleId == arrayOfWorkTime[j].PeopleId) && (arrayOfWorkTime[w].EventsDate < arrayOfWorkTime[j].EventsDate) && (arrayOfWorkTime[j].ReaderId == 3)) // удаляем лишние временные метки между приходом на работу и уходом с работы
+                            if ((arrayOfWorkTime[w].PeopleId == arrayOfWorkTime[j].PeopleId) && (arrayOfWorkTime[w].EventsDate < arrayOfWorkTime[j].EventsDate) && (Variables.inOffice.ToList().IndexOf(arrayOfWorkTime[j].ReaderId) != -1)) // удаляем лишние временные метки между приходом на работу и уходом с работы
                             {
                                 arrayOfWorkTime.RemoveAt(j);
                                 j--;
                             }
-                            else if ((arrayOfWorkTime[w].PeopleId == arrayOfWorkTime[j].PeopleId) && (arrayOfWorkTime[j].ReaderId == 13)) // время выхода с работы
+                            else if ((arrayOfWorkTime[w].PeopleId == arrayOfWorkTime[j].PeopleId) && (Variables.outOffice.ToList().IndexOf(arrayOfWorkTime[j].ReaderId) != -1)) // время выхода с работы данные о считывателях берем из массива outOffice
                             {
                                 arrayOfPeopleWorkTime[arrayOfPeopleWorkTime.Count - 1].LeavingWork = arrayOfWorkTime[j].EventsDate;// время ухода с работы
                                 arrayOfPeopleWorkTime[arrayOfPeopleWorkTime.Count - 1].BeingAtWork = (arrayOfWorkTime[j].EventsDate - arrayOfWorkTime[w].EventsDate) < Variables.halfDay ? (arrayOfWorkTime[j].EventsDate - arrayOfWorkTime[w].EventsDate) : (arrayOfWorkTime[j].EventsDate - arrayOfWorkTime[w].EventsDate - Variables.oneHour); // вычисление времени проведенного на работе
@@ -38,7 +38,7 @@ namespace ReportOfDayWork
                 }
                 for (var d = 0; d < deviations.Count; d++)//ищем не находится ли данный пользователь в отпуске итд
                 {
-                    if ((arrayOfuser[u].Id == deviations[d].PeopleId) && (deviations[d].DevFrom <= arrayOfWorkTime[0].EventsDate) && (arrayOfWorkTime[0].EventsDate <= deviations[d].DevTo))
+                    if ((arrayOfuser[u].Id == deviations[d].PeopleId) && (deviations[d].DevFrom <= arrayOfWorkTime[0].EventsDate) && (arrayOfWorkTime[0].EventsDate <= deviations[d].DevTo)) // проверяем на id прользователя и временные рамки
                     {
                         arrayOfPeopleWorkTime[u].Deviation = Variables.deviationName.Select((i,index)=> new {i, index }).Where(n=>n.index == deviations[d].DevType).ToList()[0].i; // проходим по массиву данных об отсутствии на рабочем месте
                         break;
@@ -47,5 +47,8 @@ namespace ReportOfDayWork
             }
             return arrayOfPeopleWorkTime;
         }
+        //---------
+
+
     }
 }
--- ReportOfDayWork/ReportOfDayWork/Variables.cs
+++ ReportOfDayWork/ReportOfDayWork/Variables.cs
@@ -9,14 +9,17 @@ namespace ReportOfDayWork
 {
     public static class Variables
     {
-        public static TimeSpan halfDay = new TimeSpan(4,0,0); // пол дня 4 часа
-        public static TimeSpan oneHour = new TimeSpan(1, 0, 0); // пол дня 4 часа
+        public static TimeSpan halfDay = new TimeSpan(4,0,0); // 4 часа, используется как половина рабочего дня
+        public static TimeSpan oneHour = new TimeSpan(1, 0, 0); // 1 час
         public static List<ConnectionSettings> connectionSettings = new List<ConnectionSettings>(); // настройки поключения к БД
         public static List<User> ArrayOfUsers = new List<User>(); // массив пользователей
         public static List<Deviation> ArrayOfDeviation = new List<Deviation>(); // массив отсутсвия на рабочем месте
         public static List<WorkTime> ArrayOfWorkTime = new List<WorkTime>(); // массив отработанного времени (выгрузка из БД)
         public static List<PeopleWorkTimeDay> ArrayOfPeopleWorkTimeDay = new List<PeopleWorkTimeDay>(); // массив отработанного времени одного дня        
         public static string[] deviationName = new string[5] {"больничный", "отпуск", "командировка", "удаленная работа", "отгул"}; // причины отсутсвия на рабочем месте
+        public static uint[] inOffice = new uint[4] { 35, 17, 26, 3}; // считыватели на приход в офис 35, 17, 26 - ИнфтехМск, 3 - ИнфтехУфа
+        public static uint[] outOffice = new uint[4] { 43, 25, 34, 13 }; // считыватели на уход из офиса 43, 25, 34 - ИнфтехМск, 13 - ИнфтехУфа
 
     }
 }
+ 
